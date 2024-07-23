089c54abde6da25942f544339cf96175f576bc19
Sanbao Zheng <baobaobao369@163.com>
2024-07-23 14:56:11 +0800

    docs: outline

---
+++ docs/source/basics/001_introduction_to_mud_framework.rst
@@ -0,0 +1,2 @@
+Introduction to mud framework
+==============================
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'Muddoc'
 copyright = '2021, Graziella'
 author = 'Graziella'
 
